ac1e2f390337c8172eaba92271f94b3ce2fde72a
Ben Kiel <bpkiel@yahoo.com>
2020-01-20 13:17:20 -0800

    updated through v6

--- todo.js
+++ todo.js
@@ -3,10 +3,17 @@
 let todoList = {
     todos: [],
         displayTodos: function() {
-            if (this.todos.length > 0) {
-                console.log(this.todos);
+            if (this.todos.length === 0) {
+                console.log('You have no todos!');
             } else {
-                console.log('no todos');
+                console.log('My Todos:');
+                for (let i = 0; i < this.todos.length; i++) {
+                    if (this.todos[i].isCompleted === true) {
+                        console.log(`(x) ${this.todos[i].todoText}`);
+                    } else {
+                        console.log(`( ) ${this.todos[i].todoText}`)
+                    }
+                }
             }
         },
         addTodo: function(todo = 'blank') {
@@ -14,14 +21,41 @@ let todoList = {
                 todoText: todo,
                 isCompleted: false
             });
+            this.displayTodos();
         },
         changeTodo: function(position, newTodoText = 'blank') {
             this.todos[position].todoText = newTodoText;
+            this.displayTodos();
         },
         deleteTodo: function(position) {
             this.todos.splice(position);
+            this.displayTodos();
         },
         toggleCompleted: function(position) {
             this.todos[position].isCompleted = !this.todos[position].isCompleted;
+            this.displayTodos();
+        },
+        toggleAll: function() {
+            // if everything is true, make everything false
+            let completedTodos = 0;
+            let totalTodos = this.todos.length;
+
+            for (let i = 0; i < totalTodos; i++) {
+                if (this.todos[i].isCompleted === true) {
+                    completedTodos++;
+                }
+            }
+
+            if (completedTodos === totalTodos) {
+                for (let i = 0; i < totalTodos; i++) {
+                    this.todos[i].isCompleted = false;
+                }
+            } else {
+                for (let i = 0; i < totalTodos; i++) {
+                    this.todos[i].isCompleted = true;
+                }
+            }
+                this.displayTodos();
+            // otherwise make everything true
         }
 };
